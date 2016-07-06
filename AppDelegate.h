ed592f1930289cbd4bf871841baf13e039be76a7
Lenshen <779822644@qq.com>
2016-07-06 09:30:14 +0800

    try
    try

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LuoyiSquirrel
+//
+//  Created by 远深 on 16/7/6.
+//  Copyright © 2016年 LuoyiSquirrel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  LuoyiSquirrel
+//
+//  Created by 远深 on 16/7/6.
+//  Copyright © 2016年 LuoyiSquirrel. All rights reserved.
+//
+
+#import "ViewController.h"
+//add try
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
